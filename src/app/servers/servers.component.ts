c8a2b5fed950a30f45f980c969b0954c19959eba
dmg1801 <mongaydiego@gmail.com>
2024-01-22 18:19:20 +0100

    34. Two way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,11 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent {
 allowNewServer = false;
+serverCreationStatus = 'No server was created';
+serverName =  'TestServer';
 
 constructor(){
   setTimeout(() => {
     this.allowNewServer = true;  
   }, 2000);
+}
+
+ngOninit(){
+
+}
+
+
+onCreateServer(){
+  this.serverCreationStatus = 'Server was created!';
+}
+
+onUpdateServerName(event: Event){
+  // console.log(event);
+  this.serverName = (<HTMLInputElement>event.target).value;
 }
 
   serverId: number = 10;
