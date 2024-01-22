e21cc1a80d62d4175e6832a304313e6dfcf677fb
dmg1801 <mongaydiego@gmail.com>
2024-01-22 17:17:00 +0100

    28. Property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+allowNewServer = false;
+
+constructor(){
+  setTimeout(() => {
+    this.allowNewServer = true;  
+  }, 2000);
+}
+
   serverId: number = 10;
   serverStatus: string = 'offline';
 }
